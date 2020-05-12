1c35e655101c948f261950d0dfb2018fedbd27b6
Peter Waweru <pngesh@gmail.com>
2020-05-12 07:47:25 +0300

    Testing webhook 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'VIBRANT CREATOR api version 1.0';
+    return 'VIBRANT CREATOR api version 1.0.';
   }
 }
